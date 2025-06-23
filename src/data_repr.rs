70e4e6f130a0e2407432930fcc94e591b26591c4
Marsh Ray <marshray@live.com>
2025-06-23 20:53:36 +0000

    Regularize source file headers

--- src/data_repr.rs
+++ src/data_repr.rs
@@ -5,11 +5,26 @@
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+#![deny(elided_lifetimes_in_paths)]
+#![deny(
+    clippy::panic,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::manual_assert
+)]
+#![allow(clippy::type_complexity, clippy::assertions_on_constants)]
 #![allow(dead_code)] //? TODO for development
 #![allow(non_snake_case)] //? TODO for development
+#![allow(non_upper_case_globals)] //? TODO for development
+#![allow(noop_method_call)] //? TODO for development
+#![allow(non_camel_case_types)] //? TODO for development
 #![allow(unused_mut)] //? TODO for development
+#![allow(unused_braces)] //? TODO for development
+#![allow(unused_assignments)] //? TODO for development
 #![allow(unused_variables)] //? TODO for development
 #![allow(unused_imports)] //? TODO for development
+#![allow(unreachable_code)] //? TODO for development
+#![allow(clippy::needless_lifetimes, clippy::let_and_return)] //? TODO for development
 
 use anyhow::*;
 use serde::{Deserialize, Serialize};
--- src/parser.rs
+++ src/parser.rs
@@ -1,14 +1,30 @@
-// Copyright 2023 Marsh J. Ray
+// Copyright 2023-2025 Marsh J. Ray
 //
 // Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
 // http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
 // http://opensource.org/licenses/MIT>, at your option. This file may not be
 // copied, modified, or distributed except according to those terms.
 
+#![deny(elided_lifetimes_in_paths)]
+#![deny(
+    clippy::panic,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::manual_assert
+)]
+#![allow(clippy::type_complexity, clippy::assertions_on_constants)]
 #![allow(dead_code)] //? TODO for development
+#![allow(non_snake_case)] //? TODO for development
+#![allow(non_upper_case_globals)] //? TODO for development
+#![allow(noop_method_call)] //? TODO for development
+#![allow(non_camel_case_types)] //? TODO for development
 #![allow(unused_mut)] //? TODO for development
+#![allow(unused_braces)] //? TODO for development
+#![allow(unused_assignments)] //? TODO for development
 #![allow(unused_variables)] //? TODO for development
 #![allow(unused_imports)] //? TODO for development
+#![allow(unreachable_code)] //? TODO for development
+#![allow(clippy::needless_lifetimes, clippy::let_and_return)] //? TODO for development
 
 use std::{default, fmt::Display};
 
